4bd4a99fb3094b8e145fa7471e321ed6267dc29b
melisk12 <129781468+melisk12@users.noreply.github.com>
2024-02-25 09:57:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,8 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 500)
 pd.set_option('display.expand_frame_repr', False)
 
-#########################
-# GÖREV 1: Veriyi Hazırlama
-#########################
 
-# Adım 1: armut_data.csv dosyasınız okutunuz.
+# --------------- Data Preparing ---------------
 
 data = pd.read_csv("case_armut/armut_data.csv")
 df = data.copy()
@@ -20,37 +17,35 @@ df.info()
 df.shape
 df.describe().T
 
-# Adım 2: ServisID her bir CategoryID özelinde farklı bir hizmeti temsil etmektedir.
-# ServiceID ve CategoryID'yi "_" ile birleştirerek hizmetleri temsil edecek yeni bir değişken oluşturunuz.
+# ServiceID represents a different service for each CategoryID. We create a new variable to represent services by combining ServiceID and CategoryID with "_".
 
 df["hizmetler"] = df["ServiceId"].astype(str) + "_" + df["CategoryId"].astype(str)
 df.head()
 
 
-# Adım 3: Veri seti hizmetlerin alındığı tarih ve saatten oluşmaktadır, herhangi bir sepet tanımı (fatura vb. ) bulunmamaktadır.
-# Bunu oluşturmamız isteniyor. Bunun için;
-# Association Rule Learning uygulayabilmek için bir sepet (fatura vb.) tanımı oluşturulması gerekmektedir.
-# Burada sepet tanımı her bir müşterinin aylık aldığı hizmetlerdir.
-# Örneğin; 7256 id'li müşteri 2017'in 8.ayında aldığı 9_4, 46_4 hizmetleri bir sepeti;
-# 2017’in 10.ayında aldığı  9_4, 38_4  hizmetleri başka bir sepeti ifade etmektedir.
-# Sepetleri unique bir ID ile tanımlanması gerekmektedir.
-# Bunun için öncelikle sadece yıl ve ay içeren yeni bir date değişkeni oluşturunuz.
-# UserID ve yeni oluşturduğunuz date değişkenini "_" ile birleştirirek ID adında yeni bir değişkene atayınız.
+# Adım 3: The data set consists of the date and time the services were received, there is no basket definition (invoice, etc.).
+# We are asked to create this. For this;
+# In order to apply Association Rule Learning, a basket (invoice, etc.) definition must be created.
+# Here, the basket definition is the services that each customer receives monthly.
+# For example; The customer with ID 7256 received a basket of 9_4, 46_4 services in the 8th month of 2017;
+# The 9_4 and 38_4 services received in the 10th month of 2017 represent another basket.
+# Baskets must be identified with a unique ID.
+# To do this, first we will create a new date variable that contains only the year and month.
+# We will combine the User ID and the date variable you just created with "_" and assign it to a new variable called ID.
 
 df.dtypes
 df["CreateDate"] = pd.to_datetime(df["CreateDate"])
 df.dtypes
 
-df["New_Date"] = df["CreateDate"].dt.strftime("%Y-%m") # yıl ve ay içeren yeni bir date değişkeni
+df["New_Date"] = df["CreateDate"].dt.strftime("%Y-%m") 
 df["sepet_id"]= df["UserId"].astype(str)+"_" + df["New_Date"].astype(str)
 df.head()
 
 
-#########################
-# GÖREV 2: Birliktelik Kuralları Üretiniz.
-#########################
+# --------------- Create Association Rules ---------------
 
-# Adım 1: Aşağıdaki gibi sepet hizmet pivot table’i oluşturunuz.
+
+# Let's create the basket service pivot table
 
 # Hizmet         0_8  10_9  11_11  12_7  13_11  14_7  15_1  16_8  17_5  18_4..
 # SepetID
@@ -60,17 +55,19 @@ df.head()
 # 0_2018-04        0     0      0     0      0     1     0     0     0     0..
 # 10000_2017-08    0     0      0     0      0     0     0     0     0     0..
 
-# hizmetler sutunlarda sepetıd de satırlarda olmalıdır burda
+
 df.groupby(['sepet_id', 'hizmetler'])['hizmetler'].count().unstack().fillna(0).head()
-# 2.yol:
+
+# OR:
 invoice_product_df =df.groupby(['sepet_id', 'hizmetler'])['hizmetler'].count().unstack().fillna(0).applymap(lambda x:1 if x>0 else 0)
 invoice_product_df.head()
-# unstack() fonksiyonu çok seviyeli indeksi tek seviyeli bir indekse dönüştürür
-# unstack() işlevini kullanırken, bu işlemin sonucunda eksik veri oluşabileceğine dikkat etmelisiniz. stack() işlevi ise
-# bu işlemi tersine çevirir, yani düzleştirilmiş bir DataFrame'i tekrar çok seviyeli bir indekse dönüştürür.
-# diğer yandan 0 ve 1 yazarakta binary encode işlemi yapmış oluyoruz.
 
-# Adım 2: Birliktelik kurallarını oluşturunuz.
+# The unstack() function converts a multi-level index into a single-level index. 
+# When using the unstack() function, we should be careful that missing data may occur as a result of this operation. 
+# The stack() function reverses this process, that is, it turns a flattened DataFrame back into a multi-level index.
+# In fact, by writing 0 and 1, we are performing binary encode.
+
+# Let's create association rules.
 
 frequent_itemsets = apriori(invoice_product_df,
                             min_support=0.01,
@@ -85,7 +82,7 @@ rules[(rules["support"]>0.05) & (rules["confidence"]>0.1) & (rules["lift"]>5)]
 rules.head()
 
 
-#Adım 3: arl_recommender fonksiyonunu kullanarak en son 2_0 hizmetini alan bir kullanıcıya hizmet önerisinde bulununuz.
+# arl_recommender fonksiyonunu kullanarak en son 2_0 hizmetini alan bir kullanıcıya hizmet önerisinde bulununuz.
 
 def arl_recommender(rules_df, product_id, rec_count=1):
     sorted_rules = rules_df.sort_values("lift", ascending=False)
